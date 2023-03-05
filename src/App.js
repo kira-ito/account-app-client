add578e57d35d3b5264a0f8af70ca414d6ea8e7b
ito <kira_neymar@icloud.com>
2023-03-06 02:00:22 +0900

    なんか急にデータ取れるようになったからテスト

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import axios from 'axios'
 
 function App() {
   const [balance, setBalance] = useState(0)
+  const [income, setIncome] = useState({})
   useEffect(() => {
     fetchBalance()
+    console.log(income)
   }, [])
 
   const fetchBalance = async () => {
     console.log(1)
     const result = await axios.get('https://accout-app-server.herokuapp.com/api/balance')
+    const income = await axios.get('https://accout-app-server.herokuapp.com/api/income')
     console.log(result.data)
     setBalance(result.data)
+    console.log(income.data)
+    setIncome(income.data)
   }
 
   return (
