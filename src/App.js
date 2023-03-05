ee8eceb8c9bb3b332aa35f1a971251fc9282b1fc
ito <kira_neymar@icloud.com>
2023-03-06 01:55:34 +0900

    add

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import axios from 'axios'
 
 function App() {
   const [balance, setBalance] = useState(0)
-  useEffect(async () => {
-    console.log('https://accout-app-server.herokuapp.com/api/balance')
-    const result = await axios.get('https://accout-app-server.herokuapp.com/api/balance')
-    console.log(result.data)
+  useEffect(() => {
     fetchBalance()
   }, [])
 
   const fetchBalance = async () => {
+    console.log(1)
     const result = await axios.get('https://accout-app-server.herokuapp.com/api/balance')
     console.log(result.data)
-
     setBalance(result.data)
   }
 
   return (
     <div className="App">
       残高：{balance}
-      <h1>Hello World</h1>
+      <h1>Hello World2</h1>
     </div>
   );
 }
