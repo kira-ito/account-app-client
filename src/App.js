5cf53641f83d5fdb361d1842701bb0ce2f7acd9f
ito <kira_neymar@icloud.com>
2023-03-06 01:53:30 +0900

    add

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import axios from 'axios'
 
 function App() {
   const [balance, setBalance] = useState(0)
-  useEffect(() => {
+  useEffect(async () => {
     console.log('https://accout-app-server.herokuapp.com/api/balance')
-    const a = axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    console.log(a)
+    const result = await axios.get('https://accout-app-server.herokuapp.com/api/balance')
+    console.log(result.data)
     fetchBalance()
   }, [])
 
   const fetchBalance = async () => {
-    const test = await axios.get('/api/balance')
-    console.log(test)
     const result = await axios.get('https://accout-app-server.herokuapp.com/api/balance')
     console.log(result.data)
 
